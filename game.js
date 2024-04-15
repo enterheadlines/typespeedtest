370c26994ad78cf532d73c887481552e8a1d0ed1
enterheadlines <162852023+enterheadlines@users.noreply.github.com>
2024-04-15 20:40:57 +0900

    Update game.js

--- game.js
+++ game.js
@@ -3,12 +3,12 @@ let TIME_LIMIT = 60;
 
 // define quotes to be used
 let quotes_array = [
-  "Push yourself, because no one else is going to do it for you.",
-  "Failure is the condiment that gives success its flavor.",
-  "Wake up with determination. Go to bed with satisfaction.",
-  "It's going to be hard, but hard does not mean impossible.",
-  "Learning never exhausts the mind.",
-  "The only way to do great work is to love what you do."
+  "Bokunonamaewakenndesu.",
+  "amerikaniikimasu",
+  "anatatachiwaomoshiroi",
+  "ohayouohayouohayou",
+  "konnichiwa, hahahaha",
+  "kyakyakyakyanpufaiya"
 ];
 
 // selecting required elements
